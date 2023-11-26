f5ba6e9184d9014476708de3ee69d98fe1671556
Nathan <antnefedov33@gmail.com>
2023-11-26 18:46:11 +0300

    wrote the script to animate the mouse follow effect

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,2 +1,35 @@
 "use strict"
 
+let circles = document.querySelectorAll('.container div');
+
+let clientX = -100;
+let clientY = -100;
+
+moveCircles();
+
+function moveCircles() {
+    document.addEventListener("mousemove", (e) => {
+        clientX = e.clientX;
+        clientY = e.clientY;
+    });
+
+    gsap.set(circles, {
+        x: clientX,
+        y: clientY
+    });
+
+    let animation = () => {
+        gsap.to(circles, {
+            duration: 0.1,
+            x: clientX,
+            y: clientY,
+            stagger: {
+                amount: 0.5,
+                from: 0
+            }
+        });
+        requestAnimationFrame(animation);
+    };
+
+    requestAnimationFrame(animation);
+};
